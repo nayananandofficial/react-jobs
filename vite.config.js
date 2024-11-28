43c68e16b48cb1eacb581e8378fb23d92489ea9a
Nayan <nayan@gmail.com>
2024-11-28 23:49:27 +0530

    change api url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/api',
+        target: 'https://react-jobs-server-cwu9.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
