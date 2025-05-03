94e9db99c0aab1afef82a269a582448654372fd0
Nayan Anand <125741892+nayananandofficial@users.noreply.github.com>
2025-05-03 16:13:48 +0530

    edit: vite.config.js proxy url changed

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: `${import.meta.env.REACT_APP_API_URL}/api`,
+        target: 'https://react-jobs-server-1.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
