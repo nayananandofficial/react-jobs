104036a2e464b32d76877e2e8d69c9fbaa16cbd7
Nayan Anand <125741892+nayananandofficial@users.noreply.github.com>
2025-05-03 19:19:00 +0530

    test

--- src/components/RecentJobs.jsx
+++ src/components/RecentJobs.jsx
@@ -11,7 +11,7 @@ const RecentJobs = ({ isHome = false }) => {
     const fetchJobs = async () => {
       try {
         const apiUrl = `${import.meta.env.REACT_APP_API_URL}/api/jobs` // Always fetch all jobs
-        console.log(apiUrl);
+        console.log(REACT_APP_API_URL);
         const res = await axios.get(apiUrl);
         console.log(res.data);
         let data = Array.isArray(res.data) ? res.data : []; // Ensure data is an array
